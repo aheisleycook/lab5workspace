140908844c347910c751cf79e71e8a5d328cdc9d
aheisleycook <aheisleycook@gmail.com>
2020-12-10 10:09:15 -0800

    added base

---
+++ ahclab5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+/*
+*TODO:
+* get matching project afrom withing and payment are valid 
+*/
+bool  isValidProjectNumber(int);
+
+int main() 
+{
+
+int projectnumbersPay[10];//array for payfile projectnumver checkign mathces with wothholding
+int employeeIdNumbetsPay[10];//arrya for employeeIdNumber 
+double projectamountPay[3];//projectamountPay aray fro checking for valdiity 
+
+
+}
+bool isValidProjectNumber(int ProjNum) 
+{
+/*
+* function name isvalid projectnumbrer
+*
+*/
+    return true;
+}
